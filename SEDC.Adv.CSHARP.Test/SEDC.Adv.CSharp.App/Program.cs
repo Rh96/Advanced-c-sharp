ea661943cdecd0dc78105b43be45a41c8a6d7dcc
Rade <radehrgo96@gmail.com>
2020-06-09 12:36:31 +0200

    Test123

---
+++ SEDC.Adv.CSHARP.Test/SEDC.Adv.CSharp.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SEDC.Adv.CSharp.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
